07bc3aa092adbe928bf97793e0295f5e33490544
Gabriel Ilian <66277810+G-ilian@users.noreply.github.com>
2024-08-10 20:55:40 -0300

    feat: putting the collect data into card.

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
-import {fetchCharactersData} from './install-data/index.js';
-import { installData } from './install-data/index.js';
+import getStarwarsDatabase from './helpers/database.js';
 
-// fetchCharactersData('https://swapi.dev/api/people/1/')
-//     .then(characterDetails => console.log(characterDetails))
-//     .catch(error => console.error(error));
+async function getCharacterData(id){
+    const db = await getStarwarsDatabase();
+    console.log(db.starWars)
+    let characterData = await db.starWars.get(id);
 
-// installData();   
+    if(characterData)
+        return characterData;
+    // Criar a lógica para esse caso aqui
+}
+function fillCard(characterData){
+    console.log(characterData);
+    // const characterName = document.querySelector('.characterName');
+    // const characterHeight = document.querySelector('.characterHeight');
+    // const characterGender = document.querySelector('.characterGender');
+    // const characterHomeworld = document.querySelector('.characterHomeworld');
+    // const characterFilms = document.querySelector('.characterFilms');
+
+    // characterName.textContent = characterData.name;
+    // characterHeight.textContent = characterData.height;
+    // characterGender.textContent = characterData.gender;
+    // characterHomeworld.textContent = characterData.homeworld;
+    // characterFilms.innerHTML = '';
+    // characterData.films.forEach(film => {
+    //     const filmElement = document.createElement('li');
+    //     filmElement.textContent = film;
+    //     characterFilms.appendChild(filmElement);
+    // });
+
+    
+}
+
+const linkToInstall = document.querySelector("a[href='#']");
+linkToInstall.addEventListener('click', async () => {
+    console.log("Installing data")
+    const { installData } = await import('./install-data/index.js');
+    alert('Installing data');
+    // button.disabled = true;
+    // button.setAttribute('aria-busy', true);
+    // await installData();
+    // button.removeAttribute('aria-busy');
+});
+
+function setLoading(isLoading){
+    const submitButton = form.querySelector('button[type="submit"]');
+    submitButton.setAttribute('aria-busy', isLoading);
+    submitButton.disabled = isLoading;
+}
+
+
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    const characterId = form.elements['characterId'].value;
+    console.log(`Fetching data for character ${characterId}`);
+    const characterData = await getCharacterData(characterId);
+    fillCard(characterData);
+    setLoading(false);
+});
